815d62424329194856da5bd8d616772941dbe53a
HenriqueMartins-dot <henrique.prodam@hotmail.com>
2026-05-28 13:12:57 -0300

    ok

--- main.js
+++ main.js
@@ -8,50 +8,40 @@ let nextProcess;
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1400,
-    height: 900,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: false
-    }
+    height: 900
   });
 
   mainWindow.loadURL("http://localhost:3000");
 }
 
 app.whenReady().then(() => {
-  const isDev = !app.isPackaged;
-
-  if (isDev) {
+  if (!app.isPackaged) {
     createWindow();
     return;
   }
 
-const nextBinary = path.join(
-  app.getAppPath(),
-  "node_modules",
-  ".bin",
-  process.platform === "win32" ? "next.cmd" : "next"
-);
+  const nextCli = path.join(
+    process.resourcesPath,
+    "app",
+    "node_modules",
+    "next",
+    "dist",
+    "bin",
+    "next"
+  );
 
-nextProcess = spawn(
-  nextBinary,
-  ["start"],
-  {
-    cwd: app.getAppPath(),
-    shell: true
-  }
-);
+  nextProcess = spawn(
+    process.execPath,
+    [nextCli, "start"],
+    {
+      cwd: process.resourcesPath + "/app",
+      shell: true
+    }
+  );
 
-nextProcess.stdout.on("data", data => {
-  console.log(data.toString());
-});
-
-nextProcess.stderr.on("data", data => {
-  console.log(data.toString());
-});
   setTimeout(() => {
     createWindow();
-  }, 8000);
+  }, 10000);
 });
 
 app.on("window-all-closed", () => {
@@ -59,7 +49,5 @@ app.on("window-all-closed", () => {
     nextProcess.kill();
   }
 
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
