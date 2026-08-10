3e6c525f342a09b4b22614250a24d2a49e947187
HenriqueMartins-dot <henrique.prodam@hotmail.com>
2026-08-10 13:05:21 -0300

    ok

--- main.js
+++ main.js
@@ -3,58 +3,24 @@ const { spawn } = require("child_process");
 const path = require("path");
 
 let mainWindow;
-let nextProcess;
+let backendProcess;
+let frontendProcess;
 
 app.setName("BarbieriApp");
 
 const rootPath = app.isPackaged ? process.resourcesPath + "/app" : __dirname;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1400,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    height: 900,
-    minWidth: 1100,
-    minHeight: 720,
-    backgroundColor: "#f4f8ff",
-    title: "BarbieriApp",
-    titleBarStyle: "default",
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: false,
-    },
-  });
-
-  win.setMenuBarVisibility(false);
-  win.loadURL("http://localhost:3000");
-=======
-    height: 900
-=======
-    height: 900,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: false
-    }
->>>>>>> parent of 815d624 (ok)
-  });
-
-  mainWindow.loadURL("http://localhost:3000");
->>>>>>> parent of b9ff1cf (ok)
-}
 
 function startBackend() {
   if (backendProcess) {
     return;
   }
 
-  const backendCommand = app.isPackaged
-    ? ["npm", "start"]
-    : ["npm", "run", "dev"];
-
-  backendProcess = spawn("cmd.exe", ["/c", ...backendCommand], {
+  const cmd = process.platform === "win32" ? "npm.cmd" : "npm";
+  backendProcess = spawn(cmd, ["run", "dev"], {
     cwd: path.join(rootPath, "barbieri_api"),
-    shell: true,
+    env: process.env,
+    shell: false,
+    stdio: "inherit",
   });
 }
 
@@ -63,103 +29,63 @@ function startFrontend() {
     return;
   }
 
-  const frontendCommand = app.isPackaged
-    ? ["npm", "start"]
-    : ["npm", "run", "dev"];
-
-  frontendProcess = spawn("cmd.exe", ["/c", ...frontendCommand], {
+  const cmd = process.platform === "win32" ? "npm.cmd" : "npm";
+  frontendProcess = spawn(cmd, ["run", "dev"], {
     cwd: rootPath,
-    shell: true,
+    env: process.env,
+    shell: false,
+    stdio: "inherit",
+  });
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 720,
+    backgroundColor: "#f4f8ff",
+    title: "BarbieriApp",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
   });
 }
 
 app.whenReady().then(() => {
-<<<<<<< HEAD
   if (!app.isPackaged) {
     startBackend();
     startFrontend();
-
-    setTimeout(() => {
-      createWindow();
-    }, 3000);
-    return;
   }
 
-<<<<<<< HEAD
-  startBackend();
-  startFrontend();
-=======
-  const nextCli = path.join(
-    process.resourcesPath,
-    "app",
-    "node_modules",
-    "next",
-    "dist",
-    "bin",
-    "next"
-  );
-
-  nextProcess = spawn(
-    process.execPath,
-    [nextCli, "start"],
-    {
-      cwd: process.resourcesPath + "/app",
-      shell: true
-    }
-  );
->>>>>>> parent of b9ff1cf (ok)
-=======
-  const isDev = !app.isPackaged;
-
-  if (isDev) {
-    createWindow();
-    return;
-  }
-
-const nextBinary = path.join(
-  app.getAppPath(),
-  "node_modules",
-  ".bin",
-  process.platform === "win32" ? "next.cmd" : "next"
-);
-
-nextProcess = spawn(
-  nextBinary,
-  ["start"],
-  {
-    cwd: app.getAppPath(),
-    shell: true
-  }
-);
->>>>>>> parent of 815d624 (ok)
-
-nextProcess.stdout.on("data", data => {
-  console.log(data.toString());
-});
-
-nextProcess.stderr.on("data", data => {
-  console.log(data.toString());
-});
-  setTimeout(() => {
-    createWindow();
-  }, 8000);
+  createWindow();
 });
 
 app.on("window-all-closed", () => {
-  if (nextProcess) {
-    nextProcess.kill();
+  if (backendProcess) {
+    backendProcess.kill();
   }
 
-<<<<<<< HEAD
-  app.quit();
+  if (frontendProcess) {
+    frontendProcess.kill();
+  }
+
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-=======
-  if (process.platform !== "darwin") {
-    app.quit();
->>>>>>> parent of 815d624 (ok)
   }
 });
